75ba5eaea99e27abc03d12f53f4283356e5629f8
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-22 00:27:55 +0800

    Done checking the values

--- Check_First_Last_Number.py
+++ Check_First_Last_Number.py
@@ -12,5 +12,8 @@ def check_first_last_equal(numbers):
 # Give the values
 numbers_x=[10, 20, 30, 40, 10]
 numbers_y=[75, 65, 35, 75, 30]
+
 # Check the values
+check_first_last_equal(numbers_x)
+check_first_last_equal(numbers_y)
 # Print result
