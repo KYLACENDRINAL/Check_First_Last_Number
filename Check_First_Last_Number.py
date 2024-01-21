42613b288625abb2e213f43754bfef750fc10e80
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-22 00:23:52 +0800

    Done defining the function to check first and last number

---
+++ Check_First_Last_Number.py
@@ -0,0 +1,14 @@
+# Check if the first and last number of a list is the same
+
+# pseudocode
+
+# Define the function to check first and last number
+def check_first_last_equal(numbers):
+    if numbers[0]==numbers[-1]:
+        result=True
+    else:
+        result=False
+
+# Give the values
+# Check the values
+# Print result
